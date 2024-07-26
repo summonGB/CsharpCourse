51333d2e6764d80c6286ec5d75deb0dd44e2afc3
Gabriel Barbosa <gabrielemail060@gmail.com>
2024-07-26 11:47:01 -0300

    Estudos_26/07

---
+++ Exercicio_01_CalculoAreaTerreno/Exercicio_01_CalculoAreaTerreno/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double comp, larg, area, preco, areaF, precoF;
+
+        
+        Console.WriteLine("CALCULADORA DE AREA E PREÇO DE LOTEAMENTO");
+
+        // Comprimento
+        Console.WriteLine("Digite o comprimento do seu Terreno:");
+        comp = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        // Largura
+        Console.WriteLine("Agora Digite a Largura do seu Terreno:");
+        larg = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        // Valor
+        Console.WriteLine("Agora por favor, insira o valor por M² que a imobiliaria está cobrando atualmente:");
+        preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        // Formulas/Calculos
+        areaF = comp * larg;
+        precoF = areaF * preco;
+
+        Console.WriteLine("A area total do seu terreno é " +  areaF.ToString("F2", CultureInfo.InvariantCulture) + " M² ");
+        Console.WriteLine("Preço final para compra R$ " + precoF.ToString("F2", CultureInfo.InvariantCulture));
+
+        Console.WriteLine();
+        Console.WriteLine("OBRIGADO POR UTILIZAR A FERRAMENTA DE CALCULO DE LOTEAMENTO VOLTE SEMPRE");
+
+    }
+}
---
+++ TesteSaidaDeDados_e_Vetor/TesteSaidaDeDados_e_Vetor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Intrinsics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string UM, DOIS, TRES;
+        Console.WriteLine("TESTE DE ENTRADA DE DADOS E VETOR");
+
+        Console.WriteLine("Digite tres palavras com espaço entre elas");
+        string[] vet = Console.ReadLine().Split(' ');
+        UM = vet[0];
+        DOIS = vet[1];
+        TRES = vet[2];
+
+        Console.WriteLine(UM);
+        Console.WriteLine(DOIS);
+        Console.WriteLine(TRES);
+    }
+}
